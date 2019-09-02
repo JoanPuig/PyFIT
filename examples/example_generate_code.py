9fe3c6828beb86aa2a9c8c3a8a020bb2804e26fa
Joan Puig <joan@opengamma.com>
2019-09-02 17:37:02 +0100

    Update example_generate_code.py

--- examples/example_generate_code.py
+++ examples/example_generate_code.py
@@ -10,20 +10,24 @@ def main():
     # This sample code shows how to read the Profile object from the SDK zip file and generate the FIT.types file
 
     # Modify to fit your directory setup
+    GENERATE_FILES = True
     sdk_file = './data/SDK/FitSDKRelease_Latest.zip'
     types_file = './FIT/types.py'
     messages_file = './FIT/messages.py'
 
     profile = Profile.from_sdk_zip(sdk_file)
 
-    # This will generate the code but only print it
-    # print(TypeCodeGenerator.generate(profile))
-    # print(MessageCodeGenerator.generate(profile))
+    
 
-    # Generates the code and writes the file to disk
-    TypeCodeGenerator.generate(profile, types_file)
-    MessageCodeGenerator.generate(profile, messages_file)
+    if GENERATE_FILES:
+        # Generates the code and writes the file to disk
+        TypeCodeGenerator.generate(profile, types_file)
+        MessageCodeGenerator.generate(profile, messages_file)
+    else:
+        # This will generate the code but only print it to the screen
+        print(TypeCodeGenerator.generate(profile))
+        print(MessageCodeGenerator.generate(profile))
 
-
+        
 if __name__ == "__main__":
     main()
